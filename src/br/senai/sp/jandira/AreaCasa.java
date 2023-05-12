e072fced871c6c14532319b7a0aec8b83f2684c6
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2023-05-12 15:49:26 -0300

    aaa

--- src/br/senai/sp/jandira/AreaCasa.java
+++ src/br/senai/sp/jandira/AreaCasa.java
@@ -7,45 +7,56 @@ import javax.swing.JOptionPane;
 public class AreaCasa {
 
 	public static void main(String[] args) {
-		
+
 		Scanner teclado = new Scanner(System.in);
-		
-		String nome, resposta, nomeComodo;
+
+		String resposta, nomeComodo;
 		double larg, comp, areaTotal, areaComodo;
 		areaComodo = larg * comp;
 		
-		nome = JOptionPane.showInputDialog("Olá! Qual é o seu nome?");
-		
-		JOptionPane.showMessageDialog(null, "Certo, " + nome + ", vamos começar!");
-		
+		System.out.println("***************************");
+		System.out.println("DESCUBRA A ÁREA DE SUA CASA");
+		System.out.println("***************************");
+
 		System.out.println("Deseja calcular um cômodo? (s/n");
 		resposta = teclado.next();
-		
-		//while (resposta.equals(s)) {
-			
-			// SALA
-			System.out.println("Qual é este cômodo?");
-			
-			System.out.println("Insira a LARGURA de sua sala: ");
+
+		for (; resposta.equals("s");) {
+
+			System.out.println("Qual é o nome deste cômodo?");
+			nomeComodo = teclado.next();
+
+			System.out.println("Insira a LARGURA de " + nomeComodo+ ".");
 			larg = teclado.nextDouble();
-			System.out.println("Agora insira o COMPRIMENTO de sua sala: ");
+			System.out.println("Agora, insira o COMPRIMENTO de " +nomeComodo+ ".");
 			comp = teclado.nextDouble();
 			
 			System.out.println("");
-			System.out.println("A área de sua sala é: " +areaComodo+ "m2");
-			
-			// COZINHA
-			System.out.println("");
-			System.out.println("Certo! Insira a LARGURA de sua cozinha: ");
-			larg = teclado.nextDouble();
-			System.out.println("Agora insira o COMPRIMENTO de sua cozinha: ");
-			comp = teclado.nextDouble();
-		
-			System.out.println("");
-			System.out.println("A área de sua cozinha é: " +areaComodo+ "m2");
-			
+			System.out.println("Certo! A área deste cômodo é de: " + areaComodo + "m2");
 		}
 		
+		System.out.println("Deseja adicionar mais um cômodo?");
+		
+		if (resposta.equals("s")) {
+			
+			System.out.println("Qual é este cômodo?");
+			nomeComodo = teclado.next();
+
+			System.out.println("Insira a LARGURA de " + nomeComodo+ ".");
+			larg = teclado.nextDouble();
+			System.out.println("Agora insira o COMPRIMENTO de " +nomeComodo+ ".");
+			comp = teclado.nextDouble();
+
+			System.out.println("");
+			System.out.println("A área deste cômodo é de: " + areaComodo + "m2");
+		
+			resposta = teclado.next();
+			
+		} else {
+			System.out.println("A área total da sua casa é: " +areaTotal+ ".");
+			
+		}
+
 	}
 
 }
